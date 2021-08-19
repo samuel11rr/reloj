70d5ad52a608833264554c9f96460fd81884bed6
Samuel Ramirez <samuel_ramirez@live.com.mx>
2021-08-19 02:25:18 -0500

    settings iniciado

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const main = document.querySelector('.main');
 const hoursContainer = document.querySelector('#hours');
 const minutesContainer = document.querySelector('#minutes');
 const secondsContainer = document.querySelector('#seconds');
+const timeSeparators = document.querySelectorAll('.separator');
+const fullScreenButton = document.querySelector('#full-screen-on');
+const settingsButton = document.querySelector('#settings');
+const closeSettingsButton = document.querySelector('#close-settings');
+const iconsContainer = document.querySelector('.icons');
+const settingContainer = document.querySelector('.settings-container');
+const showSeconds = document.querySelector('#show-seconds');
+
 
 const init = () => {
   const milliseconds = new Date().toString();
   const timeout = Number( milliseconds.substr(-3) );
+  uiSettings();
 
   setTimeout(() => runInterval(), timeout);
 }
@@ -42,9 +51,9 @@ const printHour = ( hour ) => {
   setColor( hour.map( number => Number(number) ) );
 }
 
-const warningTime = '23:48:00';
-const dangerTime = '23:49:00';
-const alertTime = '23:50:00';
+const warningTime = '10:30:00';
+const dangerTime = '10:35:00';
+const alertTime = '10:40:00';
 const warningTimeArray = warningTime.split(':').map( number => Number( number ) );
 const dangerTimeArray = dangerTime.split(':').map( number => Number( number ) );
 const alertTimeArray = alertTime.split(':').map( number => Number( number ) );
@@ -76,12 +85,15 @@ const itsTime = ( currentTime, limitTime, overTime = null ) => {
     : (currentTime[0] >= limitTime[0] && currentTime[1] >= limitTime [1]);
 }
 
-
 const runInterval = () => {
   setInterval(() => currentTime(), 1000);
 }
 
-const fullScreenButton = document.querySelector('#full-screen-on');
+const uiSettings = () => {
+  if ( !document.documentElement.requestFullscreen && !document.exitFullscreen ) {
+    fullScreenButton.style.display = 'none';
+  }
+}
 
 fullScreenButton.addEventListener('click', () => {
   if (!document.fullscreenElement) {
@@ -94,6 +106,30 @@ fullScreenButton.addEventListener('click', () => {
     }
   }
 });
+
+const toggleSettings = ( isOpen ) => {
+  if ( isOpen ) {
+    iconsContainer.style.display = 'none';
+    settingContainer.style.display = 'flex';
+  } else {
+    settingContainer.style.display = 'none';
+    iconsContainer.style.display = 'block';
+  }
+}
+
+settingsButton.addEventListener('click', () => toggleSettings(true) );
+closeSettingsButton.addEventListener('click', () => toggleSettings(false) );
+
+showSeconds.addEventListener('click', () => {
+  if ( showSeconds.checked ) {
+    secondsContainer.style.display = 'block';
+    timeSeparators[1].style.display = 'block';
+  } else {
+    secondsContainer.style.display = 'none';
+    timeSeparators[1].style.display = 'none';
+  }
+})
+
 
 /**APP INITIALIZATION */
 init();
